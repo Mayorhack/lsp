0ce58f356fed75305ebc0338cb2125ed794ea0f9
Mayowa Sodipo <sodipomayowa7@gmail.com>
2024-07-18 17:46:22 +0100

    fffff

--- src/pages/vehicles/index.tsx
+++ src/pages/vehicles/index.tsx
@@ -31,7 +31,7 @@ const Vehicles = () => {
     status: "",
     color: "",
   });
-  console.log("jfjdddgffgddddfjh");
+  console.log("jfgffgddddfjh");
 
   const [appliedFilter, setAppliedFilter] = useState(false);
 
