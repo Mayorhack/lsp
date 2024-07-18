6f2f0a4fa16b3ce6dc31b11b8657cd789916232e
Mayowa Sodipo <sodipomayowa7@gmail.com>
2024-07-18 17:54:16 +0100

    testing

--- src/pages/vehicles/index.tsx
+++ src/pages/vehicles/index.tsx
@@ -31,7 +31,7 @@ const Vehicles = () => {
     status: "",
     color: "",
   });
-  console.log("jfgffgfjh");
+  console.log("jfgffg");
 
   const [appliedFilter, setAppliedFilter] = useState(false);
 
